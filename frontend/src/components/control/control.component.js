4265cc4f325b74788606dd78cd0244c70fc5aa33
ALEGOR <alegor-1902@yandex.ru>
2023-10-29 03:14:32 +0300

    small front end (make control page)

--- frontend/src/components/control/control.component.js
+++ frontend/src/components/control/control.component.js
@@ -1,14 +1,41 @@
-import React, { Component } from "react";
-import Form from "react-validation/build/form";
-import QrReader from 'react-qr-scanner'
+import React from "react";
+import styles from "./control.css";
 
 export default function Control() {
   const videoURL = `http://${window.location.hostname}:8888/api/v1/video`;
   return (
-    <div className="background">
-      <h1>CONTROLLER</h1>
-      
-      <img src={videoURL} />
+    <div className="container center-container">
+      <div className="inner-content">
+        <div className="row">
+          <div className="col-md-7">
+            <img src={videoURL} alt="Изображение" className="img-fluid video" />
+          </div>
+          <div className="col-md-5 text-center">
+            <h3>1234 - Камера конференции</h3>
+            <hr></hr>
+            <div className="joystick-container">
+              <div>
+                <button className="joystick-button">↑</button>
+              </div>
+              <div>
+                <button className="joystick-button">←</button>
+                <button className="joystick-button">X</button>
+                <button className="joystick-button">→</button>
+              </div>
+              <div>
+                <button className="joystick-button">↓</button>
+              </div>
+            </div>
+            <hr></hr>
+            <span className="text-muted text-left">
+              <ul>
+                <li>DEBUG INFO:</li>
+                <li>{videoURL}</li>
+              </ul>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/control/login.component.js
+++ frontend/src/components/control/login.component.js
@@ -8,25 +8,25 @@ export default function Login({ onHandleScan, error }) {
   }
 
   return (
-    <div className="background">
-      <Form
-        className="formSignin"
-      >
-        <div className="text-center mb-4">
-          <h1>1234</h1>
-          <h1 className="h3 mb-3 font-weight-normal">Сканируйте QR код</h1>
-        </div>
+    <div className="background center-container">
+      <div className="inner-content px-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="text-center mb-4">
+              <h1>1234</h1>
+              <h1 className="h3 mb-3 font-weight-normal">Сканируйте QR код</h1>
+            </div>
 
-        {error ? (
-          <div className="form-group">
-            <div className="alert alert-danger text-center" role="alert">
-              {error}
-           </div>
+            {error ? (
+              <div className="alert alert-danger text-center" role="alert">
+                {error}
+              </div>
+            ) : (<div></div>)}
+
+            <p className="mt-5 mb-3 text-muted text-center">ALEGOR © 2023</p>
           </div>
-        ) : (<div></div>)}
-
-        <p className="mt-5 mb-3 text-muted text-center">ALEGOR © 2023</p>
-      </Form>
+        </div>
+      </div>
     </div>
   );
 }
