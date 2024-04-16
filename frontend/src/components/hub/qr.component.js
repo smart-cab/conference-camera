80acb217403cc4b55e9a4540630ca39e1c617eb9
xALEGORx <alegor-1902@yandex.ru>
2024-04-16 16:08:41 +0300

    fix qr code url

--- frontend/src/components/hub/qr.component.js
+++ frontend/src/components/hub/qr.component.js
@@ -10,7 +10,7 @@ export default function Qr({ token }) {
             <div className="col-12 text-center">
               <h1>{process.env.REACT_APP_SCHOOL} — Камера конференции</h1>
               <div className="mt-5">
-                <QRCodeSVG value={token} size={256} />
+                <QRCodeSVG value={tokenURL} size={256} />
               </div>
               <div className="mt-5">
                 <a href={tokenURL} style={{ fontSize: "32px" }}>{token}</a>
