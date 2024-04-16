365cb0b48cdb874547e20527b164eec2d20eb4b8
xALEGORx <alegor-1902@yandex.ru>
2024-04-16 17:29:43 +0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -17,13 +17,16 @@ apt install uvcdynctrl
 read -p "Enter the ip for the service to work: " ip
 read -p "Enter the port for the service to work: " port
 read -p "Enable auto generation qr code (1 - yes / 0 - no): " autoqr
+read -p "Enter school: " school
 
 # generation .env file
 echo "IP=$ip" >> .env
 echo "AUTO_QR_CODE=$autoqr" >> .env
 echo "DEBUG=0" >> .env # disable debug on production
+echo "TOKEN_LENGTH=8" >> .env
+echo "REACT_APP_SCHOOL=$school" >> frontend/.env.production
 
 cd frontend
 npm i
 cd ../
-go build main.go
+go build .
